d603e7fbad54cffe4845290fdd6a9d4c27febf5f
YangLiuQinXue <1522442699@qq.com>
2021-08-27 17:22:05 +0800

    rem初始化,背景色设置

--- 作业/js/index.js
+++ 作业/js/index.js
@@ -2,11 +2,13 @@
     //给window设置rem
     function setFont(){
         let htmlWidth=document.querySelector('html').clientWidth;
+        let htmlHeight=document.querySelector('html').clientHeight;
         // if (htmlWidth<1024)htmlWidth=1024;
         // if (htmlWidth>1920)htmlWidth=1920;
         htmlWidth<1024&&(htmlWidth=1024);
         htmlWidth>1920&&(htmlWidth=1920);
         document.querySelector('html').style.fontSize=(htmlWidth/80+'px');
+        document.querySelector('.flax').style.height=(htmlHeight+'px');
     }
     setFont();
     window.onresize=function (){
