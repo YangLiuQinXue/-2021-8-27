0c0f3cebcb66487ef96016f07bf50ea4d3d11fe9
YangLiuQinXue <1522442699@qq.com>
2021-08-27 18:40:34 +0800

    项目基本功能

--- 作业/js/index.js
+++ 作业/js/index.js
@@ -15,3 +15,26 @@
         setFont();
     };
 }
+{
+    let button=document.querySelector('.flax .left .shuRu button');
+     // let reg=/^[0-9]*$/;
+    button.addEventListener('click',function (){
+        let name=document.getElementById('name').value;
+        let pf=document.getElementById('fen').value;
+        let xz=document.getElementById('xz').value;
+        let shu=document.querySelector('.flax .left .table table');
+        if(name&&(parseInt(pf))&&(parseInt(xz))){
+            shu.innerHTML+=
+                            `<tr class="top">
+                            <td>${name}</td>
+                            <td>${pf}</td>
+                            <td>${xz}</td>
+                            </tr>`;
+            // console.log(shu);
+        }else {
+            !name&&alert('请输入姓名');
+            !name&&alert('请输入评分');
+            !name&&alert('请输入薪资');
+        }
+    });
+}
